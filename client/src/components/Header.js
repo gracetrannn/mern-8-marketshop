ea5e5b084ed7dbe2fb527499d397014acbf05112
gracetrannn <tranthanhthaotrang@gmail.com>
2024-04-14 08:28:49 +0700

    Getting screen and Modifying the title

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
   return (
     <div className='header' >
       <h1 className='gfg'>
-        GFG Fruit & Vegetable Market
+        {/*GFG*/}Git Fruit & Vegetable Market
       </h1>
       <h3 style={{ color: "green" }}>
         Total Price: {totalPrice}
